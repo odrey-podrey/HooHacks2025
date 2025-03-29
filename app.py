ce10dcc3b0bdbc76e53ab9f422a7c6d91efa2140
fanofkev@terpmail.umd.edu <aegirosaurus@gmail.com>
2025-03-29 15:36:15 -0400

    start backend work

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', "POST"])
 def home():
-    return render_template('frontEnd.html')
+    if (request.method == "GET"):
+        return render_template('frontEnd.html')
 
 if __name__ == '__main__':
     app.run()
