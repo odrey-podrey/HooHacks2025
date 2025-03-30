c0da4ea125e652c90d84e01cd4748c711a93074f
fanofkev@terpmail.umd.edu <aegirosaurus@gmail.com>
2025-03-29 23:04:22 -0400

    party and district extractor

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 import lxml.html
 import bs4 as BeautifulSoup
 import mechanize
+import regex as re
 
 # Takes in zip as a string and returns the name of the representative
 def rep_from_zip(zip):
@@ -32,3 +33,50 @@ def rep_from_zip(zip):
     
     else:
         return "invalid"
+    
+def dist_party_from_zip(zip):
+    
+    # Insantiate browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    # Open lookup link
+    br.open("https://ziplook.house.gov/")
+
+    # Find form and input zipcode
+    br.select_form(nr=1)
+    br.form["ZIP"] = zip
+    br.submit()
+    
+    # Open URL
+    URL = br.geturl()  
+    page = requests.get(URL, headers={
+    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
+
+    # Scrape HTML
+    html = lxml.html.fromstring(page.content)
+    name = html.xpath("/html/body/div[2]/div/div[2]/section/div/div[2]/div[2]/div/div/p/a[1]")
+        
+    soup: BeautifulSoup = BeautifulSoup(page.content, "html.parser")
+    
+    if (len(name) > 1): 
+        table = soup.find('div', attrs = {'id':'PossibleReps'})
+        info = table.findAll('div')[0].text.split() 
+        
+        district = ""
+        for te in info[-3:]:
+            district = district + te + " "
+        district = district[:-1]
+        party = info[-4]
+        return party, district
+        
+        
+    else:
+        table = soup.find('div', attrs = {'class':'col-xs-16 col-md-4 pull-md-12 repdistrict'}) 
+        info = table.find('p')
+        district = str.splitlines(info.text)[5]
+        dist = re.match("([^0-9]*)([0-9].*)", district).group(2)
+        party = party = str.splitlines(info.text)[12]
+        party = re.sub(r'[\t ]', "", party)
+        return party, district
+        
