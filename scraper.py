a28aed2b10d310e38b96f5057370d1787aac185f
fanofkev@terpmail.umd.edu <aegirosaurus@gmail.com>
2025-03-29 23:10:08 -0400

    code scrunching

--- scraper.py
+++ scraper.py
@@ -4,9 +4,40 @@ import bs4 as BeautifulSoup
 import mechanize
 import regex as re
 
+def from_zip(zip):
+    if (len(zip) == 5):
+    
+        # Insantiate browser
+        br = mechanize.Browser()
+        br.set_handle_robots(False)
+
+        # Open lookup link
+        br.open("https://ziplook.house.gov/")
+
+        # Find form and input zipcode
+        br.select_form(nr=1)
+        br.form["ZIP"] = zip
+        br.submit()
+        
+        # Open URL
+        URL = br.geturl()  
+        return URL
+    else:
+        return "invalid"
+
 # Takes in zip as a string and returns the name of the representative
 def rep_from_zip(zip):
 
+    URL = from_zip(zip)
+    if URL == "invalid":
+        return URL
+    else:
+        page = requests.get(URL, headers={
+        "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
+        # Scrape HTML
+        html = lxml.html.fromstring(page.content)
+        name = html.xpath("/html/body/div[2]/div/div[2]/section/div/div[2]/div[2]/div/div/p/a[1]")
+        return(name[0].text)
     if (len(zip) == 5):
     
         # Insantiate browser
